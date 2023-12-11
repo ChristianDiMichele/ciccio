34d474f0f39e6e8e1fa8a261d0e8f3652df27898
Francesco Pasqua <pasqua.francesco1@outlook.it>
2023-12-11 21:44:49 +0100

    Iaramadonna

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ["./src/**/*.{html,js,css}"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
